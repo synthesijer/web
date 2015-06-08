75ebeb8f2f4da37e22f64679bd771a7272fe7497
Takefumi MIYOSHI <miyo@wasamon.net>
2015-06-08 18:55:43 +0900

    :)

--- dl/setup_20150608.sh
+++ dl/setup_20150608.sh
@@ -31,17 +31,17 @@ echo "source $HOME/synthesijer/synthesijer_env.sh"
 echo " or"
 echo "source $HOME/synthesijer/synthesijer_env.csh"
 
-touch synthesijer-env_$VERSION.sh
-echo "export SYNTHESIJER=$HOME/synthesijer/synthesijer_$VERSION.jar" > synthesijer-env_$VERSION.sh
-echo "export SYNTHESIJER_LIB=$HOME/synthesijer/synthesijer_lib_$VERSION" >> synthesijer-env_$VERSION.sh
-echo "export SYNTHESIJER_EXTRA_LIBS=$HOME/synthesijer/synthesijer_extra-libs_$VERSION" >> synthesijer-env_$VERSION.sh
-ln -sf synthesijer-env_$VERSION.sh synthesijer-env.sh
+touch synthesijer_env_$VERSION.sh
+echo "export SYNTHESIJER=$HOME/synthesijer/synthesijer_$VERSION.jar" > synthesijer_env_$VERSION.sh
+echo "export SYNTHESIJER_LIB=$HOME/synthesijer/synthesijer_lib_$VERSION" >> synthesijer_env_$VERSION.sh
+echo "export SYNTHESIJER_EXTRA_LIBS=$HOME/synthesijer/synthesijer_extra-libs_$VERSION" >> synthesijer_env_$VERSION.sh
+ln -sf synthesijer_env_$VERSION.sh synthesijer_env.sh
 
-touch synthesijer-env_$VERSION.csh
-echo "setenv SYNTHESIJER $HOME/synthesijer/synthesijer_$JAR_VERSION.jar" > synthesijer-env_$VERSION.csh
-echo "setenv SYNTHESIJER_LIB $HOME/synthesijer/synthesijer_lib_$VERSION" >> synthesijer-env_$VERSION.csh
-echo "setenv SYNTHESIJER_EXTRA_LIBS $HOME/synthesijer/synthesijer_extra-libs_$VERSION" >> synthesijer-env_$VERSION.csh
-ln -sf synthesijer-env_$VERSION.csh synthesijer-env.csh
+touch synthesijer_env_$VERSION.csh
+echo "setenv SYNTHESIJER $HOME/synthesijer/synthesijer_$JAR_VERSION.jar" > synthesijer_env_$VERSION.csh
+echo "setenv SYNTHESIJER_LIB $HOME/synthesijer/synthesijer_lib_$VERSION" >> synthesijer_env_$VERSION.csh
+echo "setenv SYNTHESIJER_EXTRA_LIBS $HOME/synthesijer/synthesijer_extra-libs_$VERSION" >> synthesijer_env_$VERSION.csh
+ln -sf synthesijer_env_$VERSION.csh synthesijer_env.csh
 
 echo ""
 echo "A quick start guid and some informations are available in"

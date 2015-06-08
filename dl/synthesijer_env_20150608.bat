21f4a187dbfe05712da28a8c0fafc99ab1e019f1
Takefumi MIYOSHI <miyo@wasamon.net>
2015-06-08 20:31:05 +0900

    :)

--- dl/synthesijer_env_20150608.bat
+++ dl/synthesijer_env_20150608.bat
@@ -1,4 +1,4 @@
 set SYNTHESIJER_VERSION=20150608
 set SYNTHESIJER=%homepath%\synthesijer\synthesijer_%SYNTHESIJER_VERSION%.jar
-set SYNTHESIJER_LIB=%homepath%\synthesijer\synthesijer_lib_%SYNTEHSIJER_VERSION%
+set SYNTHESIJER_LIB=%homepath%\synthesijer\synthesijer_lib_%SYNTHESIJER_VERSION%
 set SYNTHESIJER_EXT_LIB=%homepath%\synthesijer\synthesijer_extra-libs_%SYNTHESIJER_VERSION%

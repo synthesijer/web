242694b60c8e50ca65dfa7f6c058aa7daab6f805
Takefumi MIYOSHI <miyo@wasamon.net>
2015-09-24 14:46:35 +0900

    moved release pacakges into each specified direcotry

--- dl/setup_20150711.bat
+++
@@ -1,6 +0,0 @@
-set SYNTHSIJER_WORK=%HOME%\synthesijer
-mkdir %SYNTHESIJER_WORK%
-bitsadmin.exe /TRANSFER htmlget https://synthesijer.github.io/web/dl/synthesijer_20150711.jar %SYNTHESIJER_WORK%\synthesijer_20150711.jar
-bitsadmin.exe /TRANSFER htmlget https://synthesijer.github.io/web/dl/synthesijer_ext-libs_20150711.zip %SYNTHESIJER_WORK%\synthesijer_ext-libs_20150711.zip
-bitsadmin.exe /TRANSFER htmlget https://synthesijer.github.io/web/dl/synthesijer_lib_20150711.zip %SYNTHESIJER_WORK%\synthesijer_lib_20150711.zip
-bitsadmin.exe /TRANSFER htmlget https://synthesijer.github.io/web/dl/synthesijer_sample_20150711.zip %SYNTHESIJER_WORK%\synthesijer_sample_20150711.zip
